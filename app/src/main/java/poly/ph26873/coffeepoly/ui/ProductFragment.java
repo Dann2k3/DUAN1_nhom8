f37ba3af4134fdf5d100c8afa64f963484d84848
minhto123 <minhtvph26873@fpt.edu.vn>
2022-12-12 23:35:06 +0700

    minhtvph26873 them chuc nang tim kiem trong quan li san pham

--- app/src/main/java/poly/ph26873/coffeepoly/ui/ProductFragment.java
+++ app/src/main/java/poly/ph26873/coffeepoly/ui/ProductFragment.java
@@ -2,11 +2,14 @@ package poly.ph26873.coffeepoly.ui;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,7 +23,7 @@ public class ProductFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_product, container, false);
     }
 
@@ -37,5 +40,29 @@ public class ProductFragment extends Fragment {
         adapter = new HorizontalRCVAdapter(getContext());
         adapter.setData(ListData.listPrd);
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+//        menu.clear();
+        inflater.inflate(R.menu.menu_search, menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                adapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+
+        });
     }
 }
